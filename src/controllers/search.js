e44e98e8d4eddb9b10e26108072c31337427f12e
Marcus Hill <marcushill12@gmail.com>
2017-03-04 17:51:23 -0600

    run eslint --fix on build

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -7,7 +7,7 @@ const app = express();
 // We can keep one of these since it doesn't require a user token
 const searcher = new Searcher();
 const search = createCachedFunction(searcher.search,
-   {context: searcher, namespace: "searchController"});
+   {context: searcher, namespace: 'searchController'});
 
 app.get('/', function(req, res) {
   search(req.query.q, req.query.page)
