d755182793493faa3d77ba9a1efd4aec1a1e9a65
Marcus Hill <marcushill12@gmail.com>
2017-03-12 14:54:48 -0500

    Fix typo in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,4 +4,4 @@ set -x # Show the output of the following commands (useful for debugging)
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_7fecd50c2af5_key -iv $encrypted_7fecd50c2af5_iv -in troubadour_key.enc -out troubadour_key -drm troubadour_key.enc
 chmod 600 troubadour_key
-mv dtroubadour_key ~/.ssh/id_rsa
+mv troubadour_key ~/.ssh/id_rsa
