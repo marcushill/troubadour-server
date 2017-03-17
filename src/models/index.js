4b424fd74dceb1550930e29a22485564b60220c9
Marcus Hill <marcushill12@gmail.com>
2017-03-17 14:50:58 -0500

    Fix eslint errors

--- src/models/index.js
+++ src/models/index.js
@@ -1,24 +1,23 @@
 // from the express-sequelize example app;
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var env       = process.env.NODE_ENV || "development";
+let fs = require('fs');
+let path = require('path');
+let Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(process.env.CONNECTION_STRING);
-var db        = {};
+let sequelize = new Sequelize(process.env.CONNECTION_STRING);
+let db = {};
 
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== "index.js");
+    return (file.indexOf('.') !== 0) && (file !== 'index.js');
   })
   .forEach(function(file) {
-    var model = sequelize.import(path.join(__dirname, file));
+    let model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
+  if ('associate' in db[modelName]) {
     db[modelName].associate(db);
   }
 });
