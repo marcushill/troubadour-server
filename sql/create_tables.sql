7feb7f9f0dc2eba8794bb5084d12de0b86594e94
Marcus Hill <marcushill12@gmail.com>
2017-03-20 09:39:08 -0500

    Adds a unique constraint in the DB to prevent duplicate preferences

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -14,7 +14,8 @@ CREATE TABLE preference (
   preference_id serial PRIMARY KEY,
   user_id text REFERENCES troubadour_user ON DELETE CASCADE,
   spotify_uri text,
-  name text -- more for our use than for display as we're pull this from Spotify
+  name text, -- more for our use than for display as we're pull this from Spotify
+  CONSTRAINT user_uri_unique UNIQUE(user_id, spotify_uri)
 );
 
 CREATE TABLE playlist (
