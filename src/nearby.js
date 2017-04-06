3b6be436c0d6c97cf6052f8e1940173845bde0d4
Marcus Hill <marcushill@users.noreply.github.com>
2017-04-06 15:58:25 -0500

    Fix a bug in the nearby query that it doesn't actually bother to check that the preferences are in fact nearby. (#45)
    Lesson learned: Don't forget to test your join conditions

--- src/nearby.js
+++ src/nearby.js
@@ -3,10 +3,11 @@ import {database as db} from './startup';
 export class Nearby {
   async getPreferences(location, maxDistance=30) {
     let preferences = await db.sequelize.query(`
-      SELECT DISTINCT pr.*
-      FROM troubadour_user u, preference pr
-      WHERE ST_DWITHIN(u.last_location,
-          ST_SetSRID(ST_MakePoint(:latitude, :longitude),4326), :maxDistance);
+    SELECT DISTINCT pr.*
+          FROM troubadour_user u, preference pr
+          WHERE ST_DWITHIN(u.last_location,
+              ST_SetSRID(ST_MakePoint(:latitude, :longitude), 4326),
+                         :maxDistance) AND u.user_id=pr.user_id;
     `, {
           model: db.Preference,
           replacements: {
